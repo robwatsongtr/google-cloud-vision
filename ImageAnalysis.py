28ed8aa5484d50c3ee53ea620fc272b404a2545a
robwatsongtr <rwatso@gmail.com>
2023-10-30 18:02:08 -0700

    yay, got class working

--- ImageAnalysis.py
+++ ImageAnalysis.py
@@ -5,28 +5,33 @@ from google.cloud import storage
 import google.api_core.exceptions
 
 class ImageAnalysis:
+    # constructor: 
     def __init__(self, bucket_name, credentials_path='polished-studio-402920-41100c8f0b19.json'):
         self.bucket_name = bucket_name
         self.credentials_path = credentials_path
-        # stores the vision client for use after initializaion
+        # stores the vision client for use in instance
         self.vision_client = self._initialize_vision_client()
-        # stores the storage client for use after initialization
+        # stores the storage client for use in instance
         self.storage_client = self._initialize_storage_client()
-
 
     def _initialize_vision_client(self):
         credentials = service_account.Credentials.from_service_account_file(self.credentials_path)
-        # Initialize the Google Cloud Vision client
         vision_client = vision.ImageAnnotatorClient(credentials = credentials)
 
         return vision_client
 
     def _initialize_storage_client(self):
         credentials = service_account.Credentials.from_service_account_file(self.credentials_path)
-        # Initialize the Google Cloud Storage client
         storage_client = storage.Client(credentials = credentials)
 
         return storage_client
+
+    def is_image_blob(self, blob):
+        valid_image_extensions = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff"}
+        # Get the file extension from the blob's name
+        file_extension = os.path.splitext(blob.name)[1].lower()
+        
+        return file_extension in valid_image_extensions
 
     def analyze_image(self, image_blob_name):
         try:
@@ -46,6 +51,29 @@ class ImageAnalysis:
         except google.api_core.exceptions.GoogleAPICallError as e:
             print(f"Google Cloud Vision API Error: {str(e)}")
             return []
+
+    def bulk_analyze_images_to_console(self, bucket_path):
+        try:
+            bucket = self.storage_client.get_bucket(self.bucket_name)
+            subdirectory_path = bucket_path
+
+            for blob in bucket.list_blobs():
+                if blob.name.startswith(subdirectory_path) and self.is_image_blob(blob):
+                    print(f"File name: {blob.name}")
+                    labels = self.analyze_image(blob.name)
+                    if labels:
+                        print("Labels detected:")
+                        for label in labels:
+                            print(label)
+                    else:
+                        print("No labels detected")
+                    print()
+
+        except Exception as e:
+            # Handle exceptions here, e.g., log the error or return an error message
+            print(f"An error occurred: {str(e)}")
+
+  
 
 
 
--- run_ImageAnalysis.py
+++ run_ImageAnalysis.py
@@ -1,18 +1,25 @@
-# driver code for ImageAnalysis class
-
 from ImageAnalysis import ImageAnalysis
 
 if __name__ == "__main__":
-    # analyze a single image: 
+    # # analyze a single image: 
+    # img_analyzer = ImageAnalysis(bucket_name = "photo-bucket_polished-studio-402920")
+    # image_blob_name = 'imgs_for_tagging/IMG_4873.jpg'
+    # labels = img_analyzer.analyze_image(image_blob_name)
+
+    # print(image_blob_name)
+
+    # if labels:
+    #     for label in labels:
+    #         print(label)
+    # else: 
+    #     print("No labels detected")
+
+    # print()
+
+    # analyze directory of images to console:
     img_analyzer = ImageAnalysis(bucket_name = "photo-bucket_polished-studio-402920")
-    image_blob_name = 'imgs_for_tagging/IMG_4873.jpg'
 
-    labels = img_analyzer.analyze_image(image_blob_name)
+    bucket_path = 'imgs_for_tagging/'
 
-    if labels:
-        for label in labels:
-            print(label)
-    else: 
-        print("No labels detected")
-
+    img_analyzer.bulk_analyze_images_to_console(bucket_path)
     
